b5d3a81fc2000c29f7490a65bdaf6f9aee796107
cjobe <cjobe026@gmail.com>
2021-10-25 20:01:20 -0500

    feat: initial python app

---
+++ py/main_api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+class BasicRequest(Resource):
+    def get(self):
+        ts = round(time.time())
+        return {
+            'message': 'Automate all the things!',
+            'timestamp': ts
+        }
+
+api.add_resource(BasicRequest, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
