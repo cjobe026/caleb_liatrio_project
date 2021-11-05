51e762d6ca2f11815e569f3b593dc222ed92cc68
cjobe026 <52351884+cjobe026@users.noreply.github.com>
2021-11-04 20:20:45 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,14 @@ provider "aws" {
   region  = var.region
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
@@ -85,16 +93,6 @@ module "eks" {
     },
   ]
 
-}
-
-
-# Create cluster
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
 }
 
 # Kubernetes deploy
